3f09572543a8caceec08f35bd3242ee2fa13476a
Park-DongGeun <akqjqcjs7@naver.com>
2023-08-07 13:54:12 +0900

    initial

---
+++ src/test/java/com/codestates/repositorypractice/RepositoryPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.repositorypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
